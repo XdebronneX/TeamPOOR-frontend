b95ecb416f2a176f2f9f54b279d8304a48454a01
Xdebx <86518562+Xdebx@users.noreply.github.com>
2024-04-17 21:33:02 +0800

    Initial commit

--- src/components/product/ProductDetails.js
+++ src/components/product/ProductDetails.js
@@ -121,6 +121,105 @@ const ProductDetails = () => {
     setIsModalOpen(false);
   };
   const starPercentage = (product.ratings / 5) * 100;
+  // return (
+  //   <Container className="p-20" maxWidth="8xl">
+  //     {loading ? (
+  //       <Loader />
+  //     ) : error ? (
+  //       <Text color="red.500">Error: {error}</Text>
+  //     ) : (
+  //       <div className="shadow p-5">
+  //         <div className="flex flex-row space-x-5 ">
+  //           <div className="">
+  //             <Carousel
+  //               showThumbs={true}
+  //               showArrows={true}
+  //               autoPlay={true}
+  //               interval={5000}
+  //               transitionTime={500}
+  //               thumbWidth={100}
+  //               thumbHeight={80}
+  //               //   style={{
+  //               //     border: "1px solid #ccc",
+  //               //     borderRadius: "5px",
+  //               //     height: "65%",
+  //               //   }}
+  //             >
+  //               {product.images &&
+  //                 product.images.map((image) => (
+  //                   <div key={image.id}>
+  //                     <img
+  //                       src={image.url}
+  //                       alt={`Product Image - ${product.name}`}
+  //                       style={{
+  //                         maxWidth: "100%",
+  //                         maxHeight: "600px",
+  //                         width: "auto",
+  //                         height: "auto",
+  //                       }}
+  //                     />
+  //                   </div>
+  //                 ))}
+  //             </Carousel>
+  //           </div>
+
+  //           <div className="flex-1 space-y-5">
+  //             <Text className="font-extrabold text-4xl">{product.name}</Text>
+  //             <Text fontSize="xl" color="gray.500">
+  //               <Box display="flex" alignItems="center">
+  //                 {[...Array(5)].map((_, index) => (
+  //                   <FaStar
+  //                     key={index}
+  //                     color={
+  //                       index + 1 <= product.ratings ? "#FCCD00" : "gray.300"
+  //                     }
+  //                   />
+  //                 ))}
+  //                 <Box
+  //                   width={`${starPercentage}%`}
+  //                   height="1em"
+  //                   overflow="hidden"
+  //                   position="relative"
+  //                 >
+  //                   <Box
+  //                     width="100%"
+  //                     height="100%"
+  //                     position="absolute"
+  //                     top="0"
+  //                     left="0"
+  //                   />
+  //                 </Box>
+  //               </Box>
+  //             </Text>
+
+  //             <div className="flex flex-row justify-between items-center border-t py-3">
+  //               <Text className="text-3xl text-red-500">₱{product.price}</Text>
+
+  //               <Button
+  //                 className="py-2 bg-red-500 rounded-full w-50"
+  //                 onClick={addToCart}
+  //                 colorScheme="#ef4444"
+  //               >
+  //                 <Text className="text-white">ADD TO CART</Text>
+  //               </Button>
+  //             </div>
+
+  //             <Text className="font-bold">Description</Text>
+  //             <Text className="font-muted">{product.description}</Text>
+  //           </div>
+  //         </div>
+
+  //         <div className="py-5">
+  //           <h4 className="font-bold text-lg">Reviews</h4>
+
+  //           {product.reviews && product.reviews.length > 0 && (
+  //             <ListReviews reviews={product.reviews} />
+  //           )}
+  //         </div>
+  //       </div>
+  //     )}
+  //   </Container>
+  // );
   return (
     <Container className="p-20" maxWidth="8xl">
       {loading ? (
@@ -139,11 +238,6 @@ const ProductDetails = () => {
                 transitionTime={500}
                 thumbWidth={100}
                 thumbHeight={80}
-                //   style={{
-                //     border: "1px solid #ccc",
-                //     borderRadius: "5px",
-                //     height: "65%",
-                //   }}
               >
                 {product.images &&
                   product.images.map((image) => (
@@ -194,14 +288,23 @@ const ProductDetails = () => {
 
               <div className="flex flex-row justify-between items-center border-t py-3">
                 <Text className="text-3xl text-red-500">₱{product.price}</Text>
-
-                <Button
-                  className="py-2 bg-red-500 rounded-full w-50"
-                  onClick={addToCart}
-                  colorScheme="#ef4444"
-                >
-                  <Text className="text-white">ADD TO CART</Text>
-                </Button>
+                {/* Add condition to disable button if product stock is 0 */}
+                {product.stock > 0 ? (
+                  <Button
+                    className="py-2 bg-red-500 rounded-full w-50"
+                    onClick={addToCart}
+                    colorScheme="#ef4444"
+                  >
+                    <Text className="text-white">ADD TO CART</Text>
+                  </Button>
+                ) : (
+                  <Button
+                    className="py-2 bg-gray-300 rounded-full w-50"
+                    disabled
+                  >
+                    <Text className="text-white">OUT OF STOCK</Text>
+                  </Button>
+                )}
               </div>
 
               <Text className="font-bold">Description</Text>
@@ -211,7 +314,6 @@ const ProductDetails = () => {
 
           <div className="py-5">
             <h4 className="font-bold text-lg">Reviews</h4>
-
             {product.reviews && product.reviews.length > 0 && (
               <ListReviews reviews={product.reviews} />
             )}
@@ -220,6 +322,7 @@ const ProductDetails = () => {
       )}
     </Container>
   );
+
 };
 
 export default ProductDetails;
